0ae239b38ea5a4610c6089e1904abd0d13be804e
Davide Bontempelli <davide.bonte@gmail.com>
2015-10-09 15:09:45 +0200

    creato il vettore di esami

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-//224
-//
-//
+var tabella = $("tr>td>table>tbody:contains(Attività Didattiche)");
+var nodiEsami = [];
+var esami = [];
 
-function Esame(materia, crediti, voto) {
-  this.materia = materia;
-  this.crediti = crediti;
-  this.voto = voto;
+function Esame(materia, crediti, voto, data) {
+    this.materia = materia;
+    this.crediti = crediti;
+    this.voto = voto;
+    this.data = data;
+}
+
+function init() {
+    trovaEsamiFatti(tabella);
+    for (var i = 0; i < nodiEsami.length; i++) {
+        esami[i]=creaEsame($(nodiEsami[i]));
+    }
+    console.log(esami);
+}
+
+function trovaEsamiFatti(tabella) {
+    $(tabella).children('tr').each(function() {
+        if (esameFatto($(this)) === true) {
+            nodiEsami.push(this);
+        }
+    });
 }
 
 function esameFatto(riga) {
@@ -19,17 +36,38 @@ function esameFatto(riga) {
     }
 }
 
-function trovaEsamiFatti(tabella) {
-    
-    $(tabella).children('tr').each(function() {
-        if (esameFatto($(this)) === true) {
-            nodiEsami.push(this);
-        }
-    });
-    console.log(nodiEsami);
+function creaEsame(nodoEsame) {
+    var materia = "";
+    var crediti = 0;
+    var voto = 0;
+    var data = "";
+    materia = materiaDaNodo(nodoEsame);
+    crediti = parseInt(nodoEsame.find('td:nth-child(7)').text());
+    voto = parseInt(votoDaNodo(nodoEsame));
+    data = dataDaNodo(nodoEsame);
+    /*
+    console.log(materia);
+    console.log(crediti);
+    console.log(data);
+    console.log(voto);
+    console.log("----------------------");*/
+    return new Esame(materia,crediti, voto, data);
+
 }
 
+function materiaDaNodo(nodo) {
+    testo = nodo.find('td:nth-child(2)').text();
+    // return /\D+[\d]+/.exec(testo).toString();
+    return testo.substring(9).toString();
+}
 
-var tabella = $("tr>td>table>tbody:contains(Attività Didattiche)");
-var nodiEsami = [];
-trovaEsamiFatti(tabella);
+function votoDaNodo(nodo) {
+    var votoS = nodo.find('td:nth-child(10)').text();
+    return /[^-]+/.exec(votoS).toString();
+}
+
+function dataDaNodo(nodo) {
+    testo = nodo.find('td:nth-child(10)').text();
+    var sub = /[0-9]+\D+/.exec(testo).toString();
+    return testo.replace(sub,"");
+}
